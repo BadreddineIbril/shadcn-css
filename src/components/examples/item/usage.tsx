3aa4aaac4da0a8d937448f6cf5d9976494d76aa3
Badreddine Ibril <badreddineibril@gmail.com>
2025-11-15 18:08:00 +0100

    feat: add item component

---
+++ src/components/examples/item/usage.tsx
@@ -0,0 +1,21 @@
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemFooter,
+  ItemHeader,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item";
+
+<Item>
+  <ItemHeader>Item Header</ItemHeader>
+  <ItemMedia />
+  <ItemContent>
+    <ItemTitle>Item</ItemTitle>
+    <ItemDescription>Item</ItemDescription>
+  </ItemContent>
+  <ItemActions />
+  <ItemFooter>Item Footer</ItemFooter>
+</Item>;
